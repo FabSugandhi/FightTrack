4f9037bb8119128774485c45cc5a5f44b1898c29
ryley <ryleycroucher@gmail.com>
2024-08-21 11:13:45 +1000

    Update User import in auth_middleware.js

--- src/fightclub_api/middleware/auth_middleware.js
+++ src/fightclub_api/middleware/auth_middleware.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../models/user.js');
 
 const verify = async (req, res, next) => {
     let token;
