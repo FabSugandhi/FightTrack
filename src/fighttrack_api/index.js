d0f14eaf776748628169e8bdd32c091a3d11ecb7
ryley <ryleycroucher@gmail.com>
2024-08-21 13:37:01 +1000

    Update directory names

--- src/fighttrack_api/index.js
+++ src/fighttrack_api/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const cors = require('cors');
 
 dotenv.config();
 connectDB();
 
+app.use(cors());
 const app = express();
 
 app.use(express.json());
@@ -19,7 +21,7 @@ app.use('/api/auth', require('./routes/auth_routes.js'));
 app.use('/api/classes', require('./routes/class_routes'));
 app.use('/api/bookings', require('./routes/booking_routes'));
 app.use('/api/dashboard', require('./routes/dashboard_routes.js'));
-// app.use('/api/payments', require('./routes/payment_routes'));
+app.use('/api/payments', require('./routes/payment_routes.js'));
 
 app.listen(5001, err => {
     if (err) {
